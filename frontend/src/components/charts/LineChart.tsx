2bd916e87f4d313854f5e95c2c8ee5daecc5f28d
theocerutti <theo.cerutti@epitech.eu>
2022-10-15 20:13:48 +0200

    Rework LineChart

--- frontend/src/components/charts/LineChart.tsx
+++ frontend/src/components/charts/LineChart.tsx
@@ -1,43 +1,273 @@
-import React from 'react';
+import {
+  Box,
+  Button,
+  Flex,
+  Icon,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Stat,
+  StatHelpText,
+  StatLabel,
+  StatNumber,
+  Text,
+  useColorModeValue,
+} from '@chakra-ui/react';
+import Card from 'components/card/Card';
+import { MdOutlineCalendarToday } from 'react-icons/md';
+import React, { useCallback, useEffect, useState } from 'react';
 import ReactApexChart from 'react-apexcharts';
+import { formatCurrency } from '../../utils/format';
+import { zoomApexChart } from '../../utils/chart';
 
-type ChartProps = {
-  // using `interface` is also ok
-  [x: string]: any;
+export type ChartTimeline =
+  | 'ONE_MONTH'
+  | 'SIX_MONTHS'
+  | 'ONE_YEAR'
+  | 'YTD'
+  | 'ALL';
+
+const chartTimelineToString = (timeline: ChartTimeline): string => {
+  switch (timeline) {
+    case 'ALL':
+      return 'All';
+    case 'ONE_MONTH':
+      return '1M';
+    case 'ONE_YEAR':
+      return '1Y';
+    case 'SIX_MONTHS':
+      return '6M';
+    case 'YTD':
+      return 'YTD';
+    default:
+      return 'Unknown';
+  }
 };
-type ChartState = {
-  chartData: any[];
-  chartOptions: any;
+
+type LineChartProps = {
+  chartId: string;
+  resetTooltip?: any;
+  canZoom?: boolean;
+  data: { name: string; data: any[] }[];
+  tooltipTitle: string;
+  height?: number;
+  options?: any;
 };
 
-class LineChart extends React.Component<ChartProps, ChartState> {
-  constructor(props: { chartData: any[]; chartOptions: any }) {
-    super(props);
+const LineChart = ({
+  chartId,
+  resetTooltip = null,
+  canZoom = true,
+  data,
+  tooltipTitle,
+  height = 350,
+  options = {},
+}: LineChartProps) => {
+  const textColorSecondary = useColorModeValue('secondaryGrey.600', 'white');
+  const boxBg = useColorModeValue('secondaryGrey.300', 'whiteAlpha.100');
 
-    this.state = {
-      chartData: [],
-      chartOptions: {},
-    };
-  }
+  const [currentTimeline, setCurrentTimeline] = useState<ChartTimeline>('YTD');
+  const [tooltipValue, setTooltipValue] = useState({ x: 0, y: new Date() });
 
-  componentDidMount() {
-    this.setState({
-      chartData: this.props.chartData,
-      chartOptions: this.props.chartOptions,
-    });
-  }
+  const updateTimeline = useCallback(
+    (timeline: ChartTimeline) => {
+      setCurrentTimeline(timeline);
 
-  render() {
-    return (
-      <ReactApexChart
-        options={this.state.chartOptions}
-        series={this.state.chartData}
-        type='line'
-        width='100%'
-        height='100%'
-      />
-    );
-  }
-}
+      switch (timeline) {
+        case 'ONE_MONTH': {
+          const startDate = new Date();
+          startDate.setMonth(startDate.getMonth() - 1);
+          zoomApexChart(chartId, startDate, new Date());
+          break;
+        }
+        case 'SIX_MONTHS': {
+          const startDate = new Date();
+          startDate.setMonth(startDate.getMonth() - 6);
+          zoomApexChart(chartId, startDate, new Date());
+          break;
+        }
+        case 'ONE_YEAR': {
+          const startDate = new Date();
+          startDate.setFullYear(startDate.getFullYear() - 1);
+          zoomApexChart(chartId, startDate, new Date());
+          break;
+        }
+        default:
+        case 'YTD': {
+          const startDate = new Date();
+          startDate.setMonth(0);
+          startDate.setDate(1);
+          zoomApexChart(chartId, startDate, new Date());
+          break;
+        }
+        case 'ALL': {
+          zoomApexChart(chartId, new Date(data[0].data[0][0]), new Date());
+          break;
+        }
+      }
+    },
+    [data]
+  );
+
+  useEffect(() => {
+    resetTooltip(setTooltipValue);
+  }, [resetTooltip]);
+
+  useEffect(() => {
+    if (data[0].data.length > 0) {
+      updateTimeline('YTD');
+    }
+  }, [data, updateTimeline]);
+
+  const chartOptions = {
+    chart: {
+      id: chartId,
+      type: 'area',
+      height: height,
+      zoom: {
+        autoScaleYaxis: true,
+      },
+      events: {
+        mouseLeave: () => {
+          if (resetTooltip) {
+            resetTooltip(setTooltipValue);
+          }
+        },
+        mouseMove: (event: any, chartContext: any, config: any) => {
+          const seriesIndex = config.seriesIndex;
+          const dataPointIndex =
+            config.dataPointIndex === -1 ? 0 : config.dataPointIndex;
+          const w = chartContext.w;
+
+          if (seriesIndex !== -1) {
+            const data =
+              w.globals.initialSeries[seriesIndex].data[dataPointIndex];
+            setTooltipValue({ x: data[1], y: new Date(data[0]) });
+          } else {
+            if (resetTooltip) {
+              resetTooltip(setTooltipValue);
+            }
+          }
+        },
+      },
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    markers: {
+      size: 0,
+      style: 'hollow',
+    },
+    yaxis: {
+      tickAmount: 5,
+      labels: {
+        formatter: function (val: number) {
+          return formatCurrency(val);
+        },
+        style: {
+          colors: '#A3AED0',
+          fontSize: '12px',
+          fontWeight: '500',
+        },
+      },
+    },
+    xaxis: {
+      type: 'datetime',
+      min:
+        data[0].data[0] && data[0].data[0].length !== 0
+          ? new Date(data[0].data[0][0]).getTime()
+          : new Date().getTime(),
+      tickAmount: 30, // 30 days
+      tooltip: {
+        enabled: false,
+      },
+      labels: {
+        style: {
+          colors: '#A3AED0',
+          fontSize: '12px',
+          fontWeight: '500',
+        },
+      },
+    },
+    grid: {
+      show: false,
+    },
+    tooltip: {
+      custom: () => '',
+    },
+    ...options,
+  };
+
+  return (
+    <Card>
+      <Flex justify='center'>
+        {canZoom && (
+          <Menu>
+            <MenuButton
+              as={Button}
+              bg={boxBg}
+              fontSize='sm'
+              fontWeight='500'
+              color={textColorSecondary}
+              borderRadius='7px'
+            >
+              <Icon
+                as={MdOutlineCalendarToday}
+                color={textColorSecondary}
+                me='4px'
+              />
+              {chartTimelineToString(currentTimeline)}
+            </MenuButton>
+            <MenuList>
+              {(
+                [
+                  'ALL',
+                  'YTD',
+                  'ONE_YEAR',
+                  'SIX_MONTHS',
+                  'ONE_MONTH',
+                ] as ChartTimeline[]
+              ).map((timeline: ChartTimeline) => (
+                <MenuItem
+                  onClick={() => updateTimeline(timeline)}
+                  key={timeline}
+                >
+                  <Text>{chartTimelineToString(timeline)}</Text>
+                </MenuItem>
+              ))}
+            </MenuList>
+          </Menu>
+        )}
+      </Flex>
+      <Flex w='100%' flexDirection='row'>
+        <Flex w='8%' justify='center' align='center'>
+          <Stat
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+            }}
+          >
+            <StatLabel>{tooltipTitle}</StatLabel>
+            <StatNumber>{formatCurrency(tooltipValue.x)}</StatNumber>
+            <StatHelpText>{tooltipValue.y.toLocaleDateString()}</StatHelpText>
+          </Stat>
+        </Flex>
+        <Box minW='92%' minH={`${height}px`} mt='auto'>
+          <div id='chart-timeline' style={{ zIndex: 0 }}>
+            <ReactApexChart
+              // @ts-ignore
+              options={chartOptions}
+              series={data}
+              type='area'
+              width='100%'
+              height={height}
+            />
+          </div>
+        </Box>
+      </Flex>
+    </Card>
+  );
+};
 
 export default LineChart;
--- frontend/src/views/charts/ChartTotalInvested.tsx
+++ frontend/src/views/charts/ChartTotalInvested.tsx
@@ -1,27 +1,5 @@
-import {
-  Alert,
-  Box,
-  Button,
-  Flex,
-  Icon,
-  Menu,
-  MenuButton,
-  MenuItem,
-  MenuList,
-  Spinner,
-  Stat,
-  StatHelpText,
-  StatLabel,
-  StatNumber,
-  Text,
-  useColorModeValue,
-} from '@chakra-ui/react';
-import Card from 'components/card/Card';
-import { MdOutlineCalendarToday } from 'react-icons/md';
+import { Alert, Spinner } from '@chakra-ui/react';
 import React, { useCallback, useEffect, useState } from 'react';
-import ReactApexChart from 'react-apexcharts';
-import { formatCurrency } from '../../utils/format';
-import { zoomApexChart } from '../../utils/chart';
 import { useQueries } from '@tanstack/react-query';
 import {
   getInvestmentsGlobalInfoQuery,
@@ -31,38 +9,9 @@ import {
 } from '../../queries/investments';
 import { defaultQueryConfig } from '../../queries/config';
 import { GetInvestmentDto } from '@shared/investment';
-
-export type ChartTimeline =
-  | 'ONE_MONTH'
-  | 'SIX_MONTHS'
-  | 'ONE_YEAR'
-  | 'YTD'
-  | 'ALL';
-
-const chartTimelineToString = (timeline: ChartTimeline): string => {
-  switch (timeline) {
-    case 'ALL':
-      return 'All';
-    case 'ONE_MONTH':
-      return '1M';
-    case 'ONE_YEAR':
-      return '1Y';
-    case 'SIX_MONTHS':
-      return '6M';
-    case 'YTD':
-      return 'YTD';
-    default:
-      return 'Unknown';
-  }
-};
+import LineChart from '../../components/charts/LineChart';
 
 export default function ChartTotalInvested() {
-  const textColorSecondary = useColorModeValue('secondaryGrey.600', 'white');
-  const boxBg = useColorModeValue('secondaryGrey.300', 'whiteAlpha.100');
-
-  const [currentTimeline, setCurrentTimeline] = useState<ChartTimeline>('YTD');
-  const [tooltipValue, setTooltipValue] = useState({ x: 0, y: new Date() });
-
   const [lineChartData, setData] = useState<{ name: string; data: any[] }[]>([
     {
       name: 'Profit',
@@ -92,55 +41,17 @@ export default function ChartTotalInvested() {
   const dataInvestments = queries[0].data;
   const dataGlobalInfo = queries[1].data;
 
-  const updateTimeline = useCallback(
-    (timeline: ChartTimeline) => {
-      setCurrentTimeline(timeline);
-
-      switch (timeline) {
-        case 'ONE_MONTH': {
-          const startDate = new Date();
-          startDate.setMonth(startDate.getMonth() - 1);
-          zoomApexChart('line-datetime', startDate, new Date());
-          break;
-        }
-        case 'SIX_MONTHS': {
-          const startDate = new Date();
-          startDate.setMonth(startDate.getMonth() - 6);
-          zoomApexChart('line-datetime', startDate, new Date());
-          break;
-        }
-        case 'ONE_YEAR': {
-          const startDate = new Date();
-          startDate.setFullYear(startDate.getFullYear() - 1);
-          zoomApexChart('line-datetime', startDate, new Date());
-          break;
-        }
-        default:
-        case 'YTD': {
-          const startDate = new Date();
-          startDate.setMonth(0);
-          startDate.setDate(1);
-          zoomApexChart('line-datetime', startDate, new Date());
-          break;
-        }
-        case 'ALL': {
-          zoomApexChart(
-            'line-datetime',
-            new Date(lineChartData[0].data[0][0]),
-            new Date()
-          );
-          break;
-        }
+  const resetTooltip = useCallback(
+    (setTooltipValue: any) => {
+      if (dataGlobalInfo && dataGlobalInfo.data) {
+        setTooltipValue({
+          x: dataGlobalInfo.data.totalInvested,
+          y: new Date(),
+        });
       }
     },
-    [lineChartData]
+    [dataGlobalInfo]
   );
-
-  const resetTooltip = useCallback(() => {
-    if (dataGlobalInfo && dataGlobalInfo.data) {
-      setTooltipValue({ x: dataGlobalInfo.data.totalInvested, y: new Date() });
-    }
-  }, [dataGlobalInfo]);
 
   useEffect(() => {
     if (isSuccess) {
@@ -163,155 +74,17 @@ export default function ChartTotalInvested() {
     }
   }, [isSuccess, dataInvestments, lineChartData]);
 
-  useEffect(() => {
-    resetTooltip();
-  }, [resetTooltip]);
-
-  useEffect(() => {
-    if (isSuccess && !loadingCalculation && lineChartData[0].data.length > 0) {
-      updateTimeline('YTD');
-    }
-  }, [isSuccess, lineChartData, loadingCalculation, updateTimeline]);
-
-  const lineChartOptionsTotalSpent = {
-    chart: {
-      id: 'line-datetime',
-      type: 'area',
-      height: 350,
-      zoom: {
-        autoScaleYaxis: true,
-      },
-      events: {
-        mouseLeave: () => {
-          resetTooltip();
-        },
-        mouseMove: (event: any, chartContext: any, config: any) => {
-          const seriesIndex = config.seriesIndex;
-          const dataPointIndex =
-            config.dataPointIndex === -1 ? 0 : config.dataPointIndex;
-          const w = chartContext.w;
-
-          if (seriesIndex !== -1) {
-            const data =
-              w.globals.initialSeries[seriesIndex].data[dataPointIndex];
-            setTooltipValue({ x: data[1], y: new Date(data[0]) });
-          } else {
-            resetTooltip();
-          }
-        },
-      },
-    },
-    dataLabels: {
-      enabled: false,
-    },
-    markers: {
-      size: 0,
-      style: 'hollow',
-    },
-    yaxis: {
-      tickAmount: 5,
-      labels: {
-        formatter: function (val: number) {
-          return formatCurrency(val);
-        },
-        style: {
-          colors: '#A3AED0',
-          fontSize: '12px',
-          fontWeight: '500',
-        },
-      },
-    },
-    xaxis: {
-      type: 'datetime',
-      min:
-        lineChartData[0].data[0] && lineChartData[0].data[0].length !== 0
-          ? new Date(lineChartData[0].data[0][0]).getTime()
-          : new Date().getTime(),
-      tickAmount: 30, // 30 days
-      tooltip: {
-        enabled: false,
-      },
-      labels: {
-        style: {
-          colors: '#A3AED0',
-          fontSize: '12px',
-          fontWeight: '500',
-        },
-      },
-    },
-    grid: {
-      show: false,
-    },
-    tooltip: {
-      custom: () => '',
-    },
-  };
-
   if (isError) return <Alert status='error'>Can't fetch investments</Alert>;
   if (loadingCalculation) return <Spinner />;
 
   return (
-    <Card>
-      <Flex justify='center'>
-        <Menu>
-          <MenuButton
-            as={Button}
-            bg={boxBg}
-            fontSize='sm'
-            fontWeight='500'
-            color={textColorSecondary}
-            borderRadius='7px'
-          >
-            <Icon
-              as={MdOutlineCalendarToday}
-              color={textColorSecondary}
-              me='4px'
-            />
-            {chartTimelineToString(currentTimeline)}
-          </MenuButton>
-          <MenuList>
-            {(
-              [
-                'ALL',
-                'YTD',
-                'ONE_YEAR',
-                'SIX_MONTHS',
-                'ONE_MONTH',
-              ] as ChartTimeline[]
-            ).map((timeline: ChartTimeline) => (
-              <MenuItem onClick={() => updateTimeline(timeline)} key={timeline}>
-                <Text>{chartTimelineToString(timeline)}</Text>
-              </MenuItem>
-            ))}
-          </MenuList>
-        </Menu>
-      </Flex>
-      <Flex w='100%' flexDirection='row'>
-        <Flex w='8%' justify='center' align='center'>
-          <Stat
-            style={{
-              display: 'flex',
-              justifyContent: 'center',
-            }}
-          >
-            <StatLabel>Total Invested</StatLabel>
-            <StatNumber>{formatCurrency(tooltipValue.x)}</StatNumber>
-            <StatHelpText>{tooltipValue.y.toLocaleDateString()}</StatHelpText>
-          </Stat>
-        </Flex>
-        <Box minW='92%' minH='300px' mt='auto'>
-          <div id='chart-timeline' style={{ zIndex: 0 }}>
-            <ReactApexChart
-              // @ts-ignore
-              options={lineChartOptionsTotalSpent}
-              series={lineChartData}
-              type='area'
-              width='100%'
-              height={300}
-            />
-          </div>
-        </Box>
-      </Flex>
-    </Card>
+    <LineChart
+      chartId='line-datetime'
+      resetTooltip={resetTooltip}
+      canZoom={true}
+      data={lineChartData}
+      tooltipTitle='Total Invested'
+      height={300}
+    />
   );
 }
